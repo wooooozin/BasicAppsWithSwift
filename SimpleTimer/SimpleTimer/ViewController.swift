bd6ccb14ddf9313747dde97d24322dafc0b85a60
wooooozin <wooooozin@gmail.com>
2022-10-03 20:28:48 +0900

    [feat] Set Slider

--- SimpleTimer/SimpleTimer/ViewController.swift
+++ SimpleTimer/SimpleTimer/ViewController.swift
@@ -10,14 +10,40 @@ import UIKit
 class ViewController: UIViewController {
     
     //MARK: - Property
+    @IBOutlet weak var timerSlider: UISlider!
     @IBOutlet weak var mainLabel: UILabel!
     
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        configureUI()
+        
     }
-
-
+    
+    //MARK: - Action
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        let seconds = Int(sender.value)
+        mainLabel.text = "\(seconds) 초"
+    }
+    
+    
+    @IBAction func startButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func resetButtonTapped(_ sender: UIButton) {
+        mainLabel.text = "초를 선택하세요!"
+        timerSlider.setValue(30, animated: true)
+    }
+    
+    
+}
+    //MARK: - Method
+extension ViewController {
+    func configureUI() {
+        mainLabel.text = "초를 선택하세요!"
+        timerSlider.value = 30
+    }
 }
 
