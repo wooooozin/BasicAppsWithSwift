78b77d31a6c8d1886c0dd24d05bdc8d7a755d712
wooooozin <wooooozin@gmail.com>
2022-10-03 20:08:04 +0900

    [feat] Set Storyboard UI

---
+++ SimpleTimer/SimpleTimer/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  SimpleTimer
+//
+//  Created by 효우 on 2022/10/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //MARK: - Property
+    @IBOutlet weak var mainLabel: UILabel!
+    
+    //MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
