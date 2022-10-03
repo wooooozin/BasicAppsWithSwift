13d882f48a774104b40543aa64a9c02c3e501a90
wooooozin <wooooozin@gmail.com>
2022-10-03 20:52:34 +0900

    [feat] Create Timer and SystemSounds

--- SimpleTimer/SimpleTimer/ViewController.swift
+++ SimpleTimer/SimpleTimer/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
@@ -13,10 +14,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var timerSlider: UISlider!
     @IBOutlet weak var mainLabel: UILabel!
     
+    weak var timer: Timer?
+    var timerNumber: Int = 0
+    
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         configureUI()
         
     }
@@ -24,26 +28,49 @@ class ViewController: UIViewController {
     //MARK: - Action
     @IBAction func sliderChanged(_ sender: UISlider) {
         let seconds = Int(sender.value)
-        mainLabel.text = "\(seconds) 초"
+        mainLabel.text = "⏰ \(seconds) 초"
+        timerNumber = seconds
     }
     
     
     @IBAction func startButtonTapped(_ sender: UIButton) {
-        
+        setTimer()
     }
     
     @IBAction func resetButtonTapped(_ sender: UIButton) {
-        mainLabel.text = "초를 선택하세요!"
-        timerSlider.setValue(30, animated: true)
+        mainLabel.text = "초를 선택하세요 ⏰"
+        timerSlider.value = 30
+        timerNumber = 0
+        timer?.invalidate()
     }
     
     
 }
-    //MARK: - Method
+//MARK: - Method
 extension ViewController {
     func configureUI() {
-        mainLabel.text = "초를 선택하세요!"
+        mainLabel.text = "초를 선택하세요 ⏰"
         timerSlider.value = 30
+    }
+    
+    func setTimer() {
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(
+            withTimeInterval: 1.0,
+            repeats: true,
+            block: { [self] _ in
+                if timerNumber > 0 {
+                    timerNumber -= 1
+                    timerSlider.value = Float(timerNumber)
+                    mainLabel.text = "⏰ \(timerNumber) 초"
+                } else {
+                    timerNumber = 0
+                    mainLabel.text = "타이머 종료 ⏰"
+                    AudioServicesPlayAlertSound(SystemSoundID(1322))
+                    timer?.invalidate()
+                }
+            }
+        )
     }
 }
 
