b2fe96990277d38ddd19657e69dcdcda96e59d0c
wooooozin <wooooozin@gmail.com>
2022-10-04 15:28:35 +0900

    [feat] Setup Storyboard UI

--- SimpleInputText/SimpleInputText/ViewController.swift
+++ SimpleInputText/SimpleInputText/ViewController.swift
@@ -8,12 +8,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: - Property
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureUI()
+        configureButtonObservers()
     }
-
-
+    
+    //MARK: - Action
+    @IBAction func doneButtonTapped(_ sender: UIButton) {
+        
+    }
 }
 
+    //MARK: - Method
+extension ViewController {
+    func configureUI() {
+        view.backgroundColor = .systemGray5
+        doneButton.isEnabled = false
+        
+        textField.delegate = self
+        textField.keyboardType = .emailAddress
+        textField.borderStyle = .roundedRect
+        textField.clearButtonMode = .always
+        textField.returnKeyType = .next
+        textField.placeholder = "E-mail"
+    }
+    
+    func configureButtonObservers() {
+        textField.addTarget(self, action: #selector(textDidChaging), for: .editingChanged)
+    }
+    
+    @objc func textDidChaging(sender: UITextField) {
+        if sender.text == "" {
+            doneButton.isEnabled = false
+        } else {
+            doneButton.isEnabled = true
+        }
+    }
+}
+
+    //MARK: - Delegate
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        return true
+    }
+}
