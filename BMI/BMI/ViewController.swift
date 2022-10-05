fd85f353b3c5edbc9ca767a009689710740ec35a
wooooozin <wooooozin@gmail.com>
2022-10-06 07:22:30 +0900

    [feat] Create Project

---
+++ BMI/BMI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI
+//
+//  Created by 효우 on 2022/10/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
