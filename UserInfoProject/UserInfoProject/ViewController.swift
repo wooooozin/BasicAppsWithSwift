e77b04d6752fd5bdb752762cc6d4c2a7fead10da
wooooozin <wooooozin@gmail.com>
2022-10-09 20:33:44 +0900

    [feat] 프로젝트 생성

---
+++ UserInfoProject/UserInfoProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UserInfoProject
+//
+//  Created by 효우 on 2022/10/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
