113d168147b29958f1af509b3043e6c35d961620
wooooozin <wooooozin@gmail.com>
2022-10-11 16:24:53 +0900

    프로젝트 생성 및 폴더 그룹 생성

---
+++ MusicSearchProject/MusicSearchProject/Cotrollers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicSearchProject
+//
+//  Created by 효우 on 2022/10/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
